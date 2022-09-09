4b7be72f443fc49d52b5df5075104cfa42275bdc
Harrison Kangaho <harrison.kangaho28@gmail.com>
2022-09-09 09:14:48 -0700

    edits quote style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,6 @@
 int main(void)
 
 {
-	int a = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	write(2,
-		"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-		a);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
