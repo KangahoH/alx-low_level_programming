2041ab67cea49c29b1004bb7817f0cea192c537c
Harrison Kangaho <harrison.kangaho28@gmail.com>
2022-09-13 16:49:42 -0700

    function that checks for alphabetic character

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@
  * _putchar - writes the character c to stdout
  * print_alphabet_x10 - print alphabet 10 times
  * _islower - return 1 if c is lowercase, 0 otherwise
+ * _isalpha - return 1 if c is upper or lowercase, 0 otherwise
  *
  *
  **/
@@ -11,3 +12,4 @@
 int _putchar(char c);
 void print_alphabet_x10(void);
 int _islower(int);
+int _isalpha(int);
