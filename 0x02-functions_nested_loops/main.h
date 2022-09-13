40211e4b767b3e00de232683705cd6ad506bf35c
Harrison Kangaho <harrison.kangaho28@gmail.com>
2022-09-13 16:40:42 -0700

    function that checks for lowercase character

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,10 +2,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- *
+ * print_alphabet_x10 - print alphabet 10 times
+ * _islower - return 1 if c is lowercase, 0 otherwise
  *
  *
  **/
 
 int _putchar(char c);
 void print_alphabet_x10(void);
+int _islower(int);
